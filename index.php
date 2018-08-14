1609b9f09423f9dfe6271a6c71fa3086f6b185aa
Maxim <legckostupov.maxim@yandex.ru>
2018-08-14 23:19:26 +0300

    composers ver. 0.1

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+if (isset($_POST['found']))
+{
+  if (isset($_POST['latitude']))
+  {
+    $lati = htmlentities($_POST['latitude']);
+  }
+  if (isset($_POST['longitude']))
+  {
+    $long = htmlentities($_POST['longitude']);
+  }
+  if (isset($_POST['adress']))
+  {
+    $adres = htmlentities($_POST['adress']);
+  }
+}
+
+$api = new Yandex\Geo\Api();
+
+// Можно искать по точке
+$api->setPoint($lati, $long);
+
+// Или можно икать по адресу
+$api->setQuery($adres);
+
+// Настройка фильтров
+$api
+    ->setLimit('') // кол-во результатов
+    ->setLang(\Yandex\Geo\Api::LANG_RU) // локаль ответа
+    ->load();
+
+$response = $api->getResponse();
+$response->getFoundCount(); // кол-во найденных адресов
+$response->getQuery(); // исходный запрос
+$response->getLatitude(); // широта для исходного запроса
+$response->getLongitude(); // долгота для исходного запроса
+
+// Список найденных точек
+$collection = $response->getList();
+foreach ($collection as $item)
+{
+    $adr = $item->getAddress(); // вернет адрес
+  $latitu = $item->getLatitude(); // широта
+  $lonitu = $item->getLongitude(); // долгота
+    $item->getData(); // необработанные данные
+}
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="ru">
+   <head>
+     <meta charset="utf-8">
+     <title>Яндекс карта</title>
+     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript">
+   </script>
+     <script type="text/javascript">
+     ymaps.ready(init);
+          function init(){
+              var myMap = new ymaps.Map("map", {
+                  center: [55.76, 37.64],
+                  zoom: 7
+              });
+              var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+                  hintContent: 'Содержимое всплывающей подсказки',
+                  balloonContent: 'Содержимое балуна'
+              });
+              myMap.geoObjects.add(myPlacemark);
+          }
+    </script>
+   </head>
+   <body>
+     <form class="" action="index.php" method="post">
+       <input type="text" name="latitude" value="" placeholder="широта">
+       <input type="text" name="longitude" value="" placeholder="долгота">
+       <input type="text" name="adress" value="" placeholder="Адрес">
+       <input type="submit" name="found" value="Найти">
+     </form> <br>
+     <table>
+       <tbody>
+         <tr>
+           <th>адрес</th>
+           <th>широта</th>
+           <th>долгота</th>
+         </tr>
+         <tr>
+           <?php foreach ($collection as $item)
+           { ?>
+           <td> <a href="index.php?<?php  ?>"><?php echo $item->getAddress() . '<br>'; ?></a> </td>
+           <td><?php echo $item->getLatitude() . '<br>'; ?></td>
+           <td><?php echo $item->getLongitude() . '<br>'; ?></td>
+         </tr>
+          <?php } ?>
+       </tbody>
+     </table>
+     <div id="map" style="width: 500px; height: 500px;"></div>
+   </body>
+ </html>
